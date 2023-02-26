8d1a108bb5e53ee5352b2e1e8d88252a67946e01
yyy1000 <69566909+yyy1000@users.noreply.github.com>
2023-02-26 22:34:19 +0800

    log: 02/26/2023

---
+++ Weekly/334.cpp
@@ -0,0 +1,48 @@
+
+// My solution
+// Sort and Double Pointers
+class Solution
+{
+public:
+    int maxNumOfMarkedIndices(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        //[ 3, 4, 5, 7]
+        int sz = nums.size();
+        int mi = sz / 2;
+        int res = 0;
+        for (int i = 0, j = mi; i < mi && j < sz;)
+        {
+            int l = nums[i], r = nums[j];
+            if (l * 2 <= r)
+            {
+                res++;
+                i++, j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        return res * 2;
+    }
+};
+
+// More Clear Solution
+// From https://leetcode.cn/problems/find-the-maximum-number-of-marked-indices/solutions/2134078/er-fen-da-an-pythonjavacgo-by-endlessche-t9f5/
+class Solution
+{
+public:
+    int maxNumOfMarkedIndices(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int i = 0, n = nums.size();
+        for (int j = (n + 1) / 2; j < n; ++j)
+            if (nums[i] * 2 <= nums[j])
+                ++i;
+        return i * 2;
+    }
+};
+
+// Summary
+// 1. 
